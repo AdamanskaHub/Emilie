3e9f6de307f8a4d2080760b783b72c68a8a1c011
Emilie G <adamanska@gmail.com>
2019-10-26 22:31:27 +0200

    :sparkles: sass set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Link } from 'gatsby'
 
+import '../sass/main.scss'
+
 export default () => {
     return(
+        <>
         <div>Hello world!!</div>
+        <div className="nav">nav</div>
+        </>
     )
 }
 
