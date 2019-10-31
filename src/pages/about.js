74c8cb2fe60aca6afa2b83a04aa791d4ce5cea17
Emilie G <adamanska@gmail.com>
2019-10-31 22:05:26 +0100

    About page almost done

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,30 +14,34 @@ const About = () => (
 
 <div className="bloc">
 <Nav />
-  <div className="title">
+  {/* <div className="title">
     <div className="title__txt">
       <b>
-        <span>A</span>
+        <span></span>
         <span className="blink">B</span>
         <span>O</span>
         <span>U</span>
-         {/* <span className="blinkless">T</span>  */}
+        <span className="blinkless">T</span>  
         <span>T</span>
       </b>
     </div>
+  </div> */}
+{/* <div className="contact-col"> */}
+<div className="content">
+  <div className="content__center">
+  <p>
+      I've worked many years as a UX/UI designer in startups and big companies.</p>
+      <p>
+      I taught design too. I've done everything from user research to front-end development, as the sole designer or in a team. </p>
+      <p>When coding I usually use React and Gatsby. I love seeing my code turning into an app/website.
+    </p>
+      <p>I'm at my best in interaction design but I love every bit of the process and can only rest once I know the experience will be enjoyable for the user.
+    </p>
+    <p>
+      You want to do an app? A platform? A dashboard? I can help you, take your idea and make it into what it should be.
+    </p>
   </div>
-
-  <div className="content">
-    <p>
-      I've worked many years as a UX/UI designer in startups and big companies.
-      I taught design too. I've done everything from user research to
-      front-end development, as the sole designer or in a team. I'm at my best
-      in interaction design but I love everybit of the process and can only rest once I know the experience will be enjoyable for the user.
-    </p>
-    <p>
-      You want to do an app? A platform? A dashboard? I can help you, take
-      your idea and make it into what it should be.
-    </p>
+    
     
   </div>
 
@@ -48,9 +52,11 @@ const About = () => (
     </div>
     </a>
   </div>
+{/* </div> */}
+  
 
-  <img src={palm} alt="palm tree" className="palm palm-left" />
-<img src={palm} alt="palm tree" className="palm palm-right" />
+  <img src={palm} alt="palm tree" className="palm palm__left" />
+<img src={palm} alt="palm tree" className="palm palm__right" />
   </div>
 
     </>
