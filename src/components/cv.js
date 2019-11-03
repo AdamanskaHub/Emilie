bf02b119184f591170d6ce14505aae9b5dd85342
Emilie G <adamanska@gmail.com>
2019-11-03 16:21:37 +0100

    About page is done. Anim & text

--- src/components/cv.js
+++ src/components/cv.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import "../sass/main.scss"
 
 const CV = props => (
-    <>
+    <div data-aos="fade-up">
         <p className="about__content--company">
             {props.company} <span className="about__content--op">{props.dates}</span>
         </p>
         <h3 className="about__content--position">{props.position}</h3>
         <p className="about__content--text">{props.content}</p>
-    </>
+    </div>
 )
 
 export default CV
--- src/pages/about.js
+++ src/pages/about.js
@@ -26,11 +26,11 @@ const About = () => {
       <Nav />
 
       <img src={tri1} alt="triangle" class="triangle triangle-1" 
-      data-aos="fade"/>
+      data-aos="fade-up"/>
       <img src={trib} alt="triangle" class="triangle triangle-big" 
-      data-aos="fade"/>
+      data-aos="fade-up"/>
       <img src={tri2} alt="triangle" class="triangle triangle-2" 
-      data-aos="fade-zoom-in" 
+      data-aos="fade-up" 
       />
       <img src={tri3} alt="triangle" class="triangle triangle-3" 
       data-aos="fade-up"/>
@@ -42,25 +42,25 @@ const About = () => {
       />
 
       <section className="about__content">
-        <h2 className="about__content--title" data-aos="fade-up">Who I am</h2>
-        <p data-aos="fade-zoom-in" >
+        <h2 className="about__content--title" data-aos="fade-down">Who I am</h2>
+        <p data-aos="fade-down">
           I've worked many years as a UX/UI designer in startups and big
           companies.
         </p>
-        <p>
+        <p data-aos="fade-down">
           I taught design too. I've done everything from user research to
           front-end development, as the sole designer or in a team.
         </p>
-        <p>
+        <p data-aos="fade-down">
           When coding I usually use React and Gatsby. I love seeing my code
           turning into an app/website.
         </p>
-        <p>
+        <p data-aos="fade-down">
           I'm at my best in interaction design but I love every bit of the
           process and can only rest once I know the experience will be enjoyable
           for the user.
         </p>
-        <p>
+        <p data-aos="fade-down">
           You want to do an app? A platform? A dashboard? I can help you, take
           your idea and make it into what it should be.
         </p>
@@ -71,33 +71,57 @@ const About = () => {
         data-aos="fade-up">What I've done</h2>
 
         <CV 
-        company="vzugzuzgu" 
-        position="gfhjkljhg"
-        dates="2012-2109"
-        text="bjfkljhkle"
-        data-aos="fade-zoom-in"
+        company="Pictet" 
+        position="Senior UX/UI"
+        dates="2019-2020"
+        text="Complex apps are my destiny and once again I'm doing UX and UI for them (and I enjoy the complexity). I also develop (jekyll) the website and the design system's website, as well as adding to its content."
         data-aos="fade-up"
         />
         <CV 
-        company="vzugzuzgu" 
-        position="gfhjkljhg"
-        dates="2012-2109"
-        text="bjfkljhkle"
+        company="Freelancing" 
+        position="Front-end developer & UX/UI designer"
+        dates="2009-forever"
+        text="I really enjoy the variety of projects that keep me sharp. Always ready for the next one!
+        I worked on multiple projects over the years. At the beginning mostly UX/UI, then I started doing front-end too. Most of the jobs where for complex interfaces for specialised users."
         data-aos="fade-up"
         />
         <CV 
-        company="vzugzuzgu" 
-        position="gfhjkljhg"
-        dates="2012-2109"
-        text="bjfkljhkle"
+        company="Kido dynamics" 
+        position="UX/UI & front-end developer"
+        dates="2019-2019"
+        text="Short but very interesting experience. Everything from scratch from design to front-end (the type of front-end that mixes with back-end). It was full React."
         />
         <CV 
-        company="vzugzuzgu" 
-        position="gfhjkljhg"
-        dates="2012-2109"
-        text="bjfkljhkle"
+        company="Precept" 
+        position="UX/UI & junior front-end developer"
+        dates="2017-2018"
+        text="My first job as a front-end dev. I had a mentor and learned a lot. It was great and I loved it. I worked on React with Redux on two different webapps. First time coding the design system."
         />
-        
+        <CV 
+        company="Swissquote" 
+        position="Interaction designer"
+        dates="2015-2016"
+        text="I worked in a team of designers. I worked on very complex interfaces (a great challenge) and learned more about team work. I also learned quite a bit about the necessity of user testing. It was my first design system creation."
+        />
+        <CV 
+        company="Nomades" 
+        position="UX/UI course instructor"
+        dates="2014-2016"
+        text="I gave the intro to design and the material design courses to people in a bootcamp to become front-end developers. I love learning, but also teaching (the other side of the learning coin). It's always a pleasure to share my knowledge."
+        />
+        <CV 
+        company="SuperAwesome" 
+        position="UX/UI designer"
+        dates="2013-2015"
+        text="A great experience that made me grow as a designer. I was alone and was doing everything from UX to UI and working close with the dev team. I worked half of the time on a web and mostly mobile app for kids, and the other half on a b2b website."
+        />
+        <CV 
+        company="Targetz" 
+        position="Co-founder & UX/UI designer"
+        dates="2012-2013"
+        text="I co-founded this startup with previous work partners. My role was to take care of the design and help with the marketing. We didn't become multimillionaires, but it was still a lot of fun."
+        />
+        <p>And more...</p>
       </section>
 
     </div>
