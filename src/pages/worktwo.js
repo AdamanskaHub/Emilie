6553bc13ce0390ab86b24808b6b9bd7bc5ba12ce
Emilie G <adamanska@gmail.com>
2019-11-13 20:42:08 +0100

    Preparing some CSS

--- src/pages/worktwo.js
+++ src/pages/worktwo.js
@@ -1,36 +1,77 @@
 import React, { Component } from "react"
 import Nav from "../components/Nav"
+// import TweenMax from "gsap/all";
+// import hover from "../hover"
+// import * as THREE from 'three';
+// import threeEntryPoint from './threejs/threeEntryPoint';
 
 import "../sass/main.scss"
-import gone from "../img/gone.svg"
-// import drinko from "../img/drinko-big.png"
 // import ucheck from "../img/ucheck.png"
 // import kid from "../img/kid.jpg"
 
-const Worktwo = () => {
-  return (
-    <div className="base work">
-      <Nav 
-      isActive="worktwo" 
-      />
-      {/* ===== Bg ===== */}
-      <div className="fixed-img">
+class Worktwo extends Component {
+
+  // componentDidMount() {
+  //   const scene = new THREE.Scene();
+  //   //fov aspect(width/height) near far
+  //   const camera = new THREE.PerspectiveCamera(
+  //     75,
+  //     window.innerWidth/window.innerHeight,
+  //     0.1, 1000 
+  //     );
+  //   const renderer = new THREE.WebGLRenderer();
+  //   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // }
+
+
+  render() {
+    return (
+      <div className="base work">
+        <Nav
+          isActive="worktwo"
+        />
+
+{/* A N I M A T I O N  */}
+<div className="container">
+  <div className="head">
+    <div className="eyer"></div>
+    <div className="eyel"></div>
+    <div className="nose"></div>
+  </div>
+
+  <div className="teeth">
+    <div className="t-one"></div>
+    <div className="t-two"></div>
+    <div className="t-three"></div>
+    <div className="t-three"></div>
+    <div className="t-two"></div>
+    <div className="t-one"></div>
+  </div>
+
+</div>
+
+        {/* ===== Bg ===== */}
+        {/* <div className="fixed-img">
         <img alt="decorative lines" class="pentagon big-pentagon" src={gone} />
-      </div>
+      </div> 
       <div className="centered-img small-img">
         <img
           alt="decorative lines"
           class="pentagon small-pentagon"
           src={gone}
         />
-      </div>
-      {/* ===== Col ===== */}
-      <div className="work--display">
-        <img src="drinko" alt="drinko"/>
+      </div>*/}
+        {/* ===== Col ===== */}
+        <div className="worktwo__distortion">
+          {/* <img src={drinko} alt="drinko"/> */}
 
+          {/* ====== C A N V A S ======  */}
+          {/* <div ref={ref => (this.mount = ref)} /> */}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Worktwo
